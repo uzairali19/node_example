3e21fc4346d8bb0e729249502f114f3f9904039a
Uzair <uzairali19b@gmail.com>
2021-10-10 22:22:15 +0500

    update callbacks in rectangle.js

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 var rect = require("./rectangle.js");
 
 function solveRect(l, b) {
-  console.log(`Solving for rectangle with the length ${l} and breath ${b}`);
-
-  if (l <= 0 || b <= 0) {
-    console.log("Please provide the length and breath greater and zero");
-  } else {
-    console.log(`The area of the rectangle is ${rect.area(l, b)}`);
-    console.log(`The area of the perimeter is ${rect.perimeter(l, b)}`);
-  }
+  console.log("Solving for rectangle with l = " + l + " and b = " + b);
+  rect(l, b, (err, rectangle) => {
+    if (err) {
+      console.log("ERROR: ", err.message);
+    } else {
+      console.log(
+        "The area of the rectangle of dimensions l = " +
+          l +
+          " and b = " +
+          b +
+          " is " +
+          rectangle.area()
+      );
+      console.log(
+        "The perimeter of the rectangle of dimensions l = " +
+          l +
+          " and b = " +
+          b +
+          " is " +
+          rectangle.perimeter()
+      );
+    }
+  });
+  console.log("This statement after the call to rect()");
 }
 
 solveRect(2, 4);
