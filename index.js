6501b98f2d07e8a588db062b9fe11a3436c3f417
Uzair <uzairali19b@gmail.com>
2021-10-11 23:55:22 +0500

    update express server with morgan

--- index.js
+++ index.js
@@ -96,19 +96,22 @@
 
 // Express server
 
-const express = require("express");
-const http = require("http");
+const express = require('express');
+const http = require('http');
+const morgan = require('morgan');
 
-const hostname = "localhost";
+const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
+app.use(morgan('dev'));
+
+app.use(express.static(__dirname + '/public'));
 
 app.use((req, res, next) => {
-  console.log(req.headers);
   res.statusCode = 200;
-  res.setHeader("Content-type", "text/html");
-  res.end("<html><body><h1>Hello World</h1></body></html>");
+  res.setHeader('Content-type', 'text/html');
+  res.end('<html><body><h1>Hello World</h1></body></html>');
 });
 
 const server = http.createServer(app);
